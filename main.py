158312fbf08df449cc34d56e9817cc57f113facc
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-10-20 18:24:27 -0400

    serverside rendered homepage

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return { "message": "hello world" }
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse('index.html', { "request": request })
