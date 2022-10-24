ca7c2764307459fcf7ee3b952243eced168d88ed
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-10-24 08:26:36 -0400

    replaced instructions with actual sentiment scores. Lightened color scheme for background

--- static/index.js
+++ static/index.js
@@ -3,6 +3,7 @@ const ws = new WebSocket("ws://localhost:8000/test")
 const editorText = document.getElementById("editor-text");
 const editorPlaceholder = document.querySelector('.editor-placeholder');
 const clickMe = document.querySelector('.click-me');
+const scoreDisplayers = Array.from(document.getElementsByClassName('score'))
 
 // --[EVENT LISTENERS]-- //
 editorText.addEventListener('keyup', (e) => {
@@ -26,14 +27,25 @@ editorText.addEventListener('focusout', (e) => {
 ws.onmessage = event => {
     const { text, NEGATIVE, NEUTRAL, POSITIVE } = JSON.parse(event.data);
 
+    // reset if editor is empty
     if (isEditorEmpty(text)) {
         document.body.style.backgroundColor = 'ghostwhite';
+        scoreDisplayers.forEach(display => display.innerHTML = null);
         return;
     }
 
     const pre_scores = [NEGATIVE, NEUTRAL, POSITIVE];
     const rgb = []
-    pre_scores.forEach(score => { rgb.push(parseFloat(score) * 255); })
+
+    // display scores
+    scoreDisplayers.forEach((display, ind) => {
+        const score = (round(+(pre_scores[ind]) * 100).toFixed(2));
+        display.innerHTML = `${score}%`;
+    })
+
+    // set rgb values
+    const lightenBy = 100;
+    pre_scores.forEach(score => { rgb.push(lightenBy + parseFloat(score) * (255 - lightenBy)); });
     
     let color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
     document.body.style.backgroundColor = color;
@@ -42,4 +54,8 @@ ws.onmessage = event => {
 // --[UTILITY FUNCTIONS]-- //
 function isEditorEmpty(text) {
     return !text || text == '<br>'
+}
+
+function round(num) {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
 }
