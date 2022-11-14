4da2cc89cd012842d5e1f2cba1e55f108ab6332b
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-11-14 16:35:31 -0500

    docker and ecs integration

--- scripts/install_app_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get -y install python3 python3-pip
-
-cd /sentitext/src
-
-sudo pip3 install --no-cache-dir -r requirements.txt
--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /sentitext/src
-
-uvicorn main:app
