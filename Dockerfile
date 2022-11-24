06d963a5aa1321152ca8e0fd249ab716f88ef530
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-11-23 21:15:08 -0500

    refactored for deployment

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.8.15-slim-buster
-WORKDIR /app
-COPY . /app
-RUN pip install -r requirements.txt
-CMD ["uvicorn", "main:app",  "--host", "0.0.0.0", "--port", "8000"]
--- api/main.py
+++ api/main.py
@@ -1,17 +1,8 @@
-from fastapi import FastAPI, Request, WebSocket
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, WebSocket
 from analyzer import analyze_text
 
 app = FastAPI()
 
-app.mount('/static', StaticFiles(directory='static'), name='static')
-templates = Jinja2Templates(directory='templates')
-
-@app.get('/', response_class=HTMLResponse)
-def read_root(request: Request):
-    return templates.TemplateResponse('index.html', { "request": request })
     
 @app.websocket("/test")
 async def test(websocket: WebSocket):
